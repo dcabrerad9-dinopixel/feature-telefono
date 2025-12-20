60533aab0216515baa39e01b919fc593109cb0da
Andree Cabrera <andree.cabrera@isoft-ste.com>
2025-12-20 09:57:31 -0600

    feat: agregar campo telÃ©fono y bordes redondeados a los inputs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import InputText from "./components/InputText";
 const App = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-
+  const [telefono, setTelefono] = useState<string>("");
+  
   return (
     <div className="container">
       <h1 style={{ color: "black" }}>Formulario</h1>
@@ -19,9 +20,14 @@ const App = () => {
         type="password"
         onChange={setPassword}
       />
+      <InputText
+        placeholder="Ingresa tu teléfono"
+        type="tel"
+        onChange={setTelefono}
+      />
       <button>Submit</button>
       <p style={{ color: "black" }}>
-        Usuario: {username} <br /> Contraseña: {password}
+        Usuario: {username} <br /> Contraseña: {password} <br /> Teléfono: {telefono}
       </p>
     </div>
   );
